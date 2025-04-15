8bc536de97b18abe2b50a6912f6db180d04aee1c
nusinji <158439807+kahandboo@users.noreply.github.com>
2025-04-16 00:14:37 +0900

    [Silver III] Title: 01타일, Time: 336 ms, Memory: 71968 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,11 +1,18 @@
-n = int(input())
-MOD = 15746
+import sys            
 
-dp = [0] * (n + 2)
-dp[1] = 1
-dp[2] = 2
+N = int(sys.stdin.readline())
+if N==1:
+    print(1)
+    exit()
+elif N==2:
+    print(2)
+    exit()
+memo = [0] * (N+1)
+memo[1] = 1
+memo[2] = 2
 
-for i in range(3, n + 1):
-    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
+for i in range(3, N+1):
+    memo[i] = (memo[i-1] + memo[i-2]) % 15746
 
-print(dp[n])
+
+sys.stdout.write(str(memo[N]))
