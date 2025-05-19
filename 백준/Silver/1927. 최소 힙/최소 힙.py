04b0e45ed50695e893fdaeab5e2dbe040fbb52b2
nusinji <158439807+kahandboo@users.noreply.github.com>
2025-05-19 21:39:54 +0900

    [Silver II] Title: 최소 힙, Time: 104 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,23 @@
+import heapq
+import sys
+
+def cal(x):
+    global answer
+    if x > 0:
+        heapq.heappush(hq, x)
+
+    if x == 0:
+        if hq:
+            answer.append(heapq.heappop(hq))
+        else:
+            answer.append(0)        
+
+N = int(input())
+hq = []
+answer = []
+
+for _ in range(N):
+    cal(int(sys.stdin.readline()))
+
+for a in answer:
+    sys.stdout.write(str(a) + '\n')
