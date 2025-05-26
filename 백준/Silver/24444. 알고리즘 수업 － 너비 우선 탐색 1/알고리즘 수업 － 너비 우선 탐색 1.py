6ba77ac155f2d50ca819d4c49d5cb5d81feadecf
nusinji <158439807+kahandboo@users.noreply.github.com>
2025-05-26 21:20:24 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 516 ms, Memory: 67768 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,54 @@
+import sys
+from collections import deque
+
+def bfs(r):
+    global count
+
+    for i in graph[r]:
+        visited[i] = False
+    
+    visited[r] = True
+    result[r] = count
+    count += 1
+
+    q = deque([r])
+
+    while q:
+        u = q.popleft()
+        result[u] = count
+        count += 1
+        
+        if u in graph:
+            graph[u].sort()
+
+            for i in graph[u]:
+                if visited[i] == False:
+                    visited[i] = True
+                    q.append(i)
+
+
+N, M, R = map(int, sys.stdin.readline().split())
+
+graph = {}
+visited = [0] * (N+1)
+result = [0] * (N+1)
+count = 0
+
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+
+    if u in graph:
+        graph[u].append(v)
+    else:
+        graph[u] = [v]
+
+    if v in graph:
+        graph[v].append(u)
+    else:
+        graph[v] = [u]
+
+bfs(R)
+
+for i in range(1, N+1):
+    sys.stdout.write(str(result[i]) + '\n')
+
