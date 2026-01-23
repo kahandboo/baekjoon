1ac986853d5e3117e28394360bccac78299a70a7
nusinji <158439807+kahandboo@users.noreply.github.com>
2026-01-23 14:42:07 +0900

    [Silver III] Title: Four Squares, Time: 120 ms, Memory: 110060 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+memo = [i for i in range(N+1)]
+
+for i in range(N+1):
+    j = 1
+    while j*j <= i:
+        if memo[i] > memo[i - j*j] + 1:
+            memo[i] = memo[i - j*j] + 1
+        j += 1
+        
+print(memo[N])
